70443a77e2fd271feda083c0c9f31abe5240d0fd
L-J-Woolf <46949570+L-J-Woolf@users.noreply.github.com>
2025-10-14 10:20:22 +0000

    new file:   assets/icon-192.png new file:   assets/icon-512.png new file:   manifest.webmanifest new file:   service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+const CACHE = "app-v3";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/script.js",
+  "/manifest.webmanifest",
+  "/assets/icon-192.png",
+  "/assets/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  const { request } = event;
+  event.respondWith(
+    caches.match(request).then(cached => cached || fetch(request))
+  );
+});
