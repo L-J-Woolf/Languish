b1430d01f1700a14d9a9f616f2dd64ea4d1ae224
L-J-Woolf <46949570+L-J-Woolf@users.noreply.github.com>
2025-10-15 06:30:47 +0000

    modified:   index.html 	modified:   pwa-service-worker.js

--- pwa-service-worker.js
+++ pwa-service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "10-15-5-26"; // update cache name when changing assets, or old caches can linger.
+const CACHE = "10-15-5-30"; // update cache name when changing assets, or old caches can linger.
 const ASSETS = [
   ".",
   "index.html",
