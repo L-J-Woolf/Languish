e66b06bc801002924a70168406967a109a748a6e
L-J-Woolf <46949570+L-J-Woolf@users.noreply.github.com>
2025-11-06 16:26:34 +1100

    update version number

--- pwa-service-worker.js
+++ pwa-service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = `251105_2115`; // update cache name when changing assets, or old caches can linger.
+const CACHE = `251106_1630`; // update cache name when changing assets, or old caches can linger.
 const ASSETS = [
   ".",
   "index.html",
